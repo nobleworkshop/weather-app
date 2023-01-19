9a6c1798f45019b4c6267711e26100c71314a353
Yurii Kluchevsky <yurijkharin@gmail.com>
2023-01-20 00:28:48 +0300

    Кастомные иконки

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import conditions from './conditions.js';
+
+console.log(conditions);
+
 const apiKey = 'e9a5d3b74bf84418b11193028231901';
 
 // Элементы на странице
@@ -18,14 +22,14 @@ function showError(errorMessage) {
 	header.insertAdjacentHTML('afterend', html);
 }
 
-function showCard({ name, country, temp, condition }) {
+function showCard({ name, country, temp, condition, imgPath }) {
 	// Разметка для карточки
 	const html = `<div class="card">
                                 <h2 class="card-city">${name} <span>${country}</span></h2>
 
                                 <div class="card-weather">
                                     <div class="card-value">${temp}<sup>°c</sup></div>
-                                    <img class="card-img" src="./img/example.png" alt="Weather">
+                                    <img class="card-img" src="${imgPath}" alt="Weather">
                                 </div>
 
                                 <div class="card-description">${condition}</div>
@@ -60,11 +64,27 @@ form.onsubmit = async function (e) {
 	} else {
 		removeCard();
 
-        const weatherData = {
+		console.log(data.current.condition.code);
+
+		const info = conditions.find(
+			(obj) => obj.code === data.current.condition.code
+		);
+		console.log(info);
+		console.log(info.languages[23]['day_text']);
+
+        const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
+		const fileName = (data.current.is_day ? info.day : info.night) + '.png';
+        const imgPath = filePath + fileName;
+        console.log('filePath', filePath + fileName);
+
+		const weatherData = {
 			name: data.location.name,
 			country: data.location.country,
 			temp: data.current.temp_c,
-			condition: data.current.condition.text,
+			condition: data.current.is_day
+				? info.languages[23]['day_text']
+				: info.languages[23]['night_text'],
+			imgPath,
 		};
 
 		showCard(weatherData);
